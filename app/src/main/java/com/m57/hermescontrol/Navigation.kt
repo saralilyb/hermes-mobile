4ecde51e2caf2077f976c00750946d253c97f6d9
Hy4ri <hiari45@gmail.com>
2026-06-19 19:47:49 +0300

    Consume window insets in NavDisplay to eliminate keyboard gaps (Issue #59)

--- app/src/main/java/com/m57/hermescontrol/Navigation.kt
+++ app/src/main/java/com/m57/hermescontrol/Navigation.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
@@ -445,6 +446,7 @@ fun MainNavigation() {
                 modifier =
                     Modifier
                         .padding(paddingValues)
+                        .consumeWindowInsets(paddingValues)
                         .fillMaxSize(),
             )
         }
